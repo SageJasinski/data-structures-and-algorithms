64e9d3de6bc978a0159b17236ec956a709e83d96
Sage Jasinski <sagejasinski@gmail.com>
2022-10-29 17:44:13 -0700

    linked-list data structures challenge and test made

--- java/datastructures/lib/src/test/java/datastructures/linkedlist/LinkedListTest.java
+++ java/datastructures/lib/src/test/java/datastructures/linkedlist/LinkedListTest.java
@@ -1,15 +1,50 @@
 package datastructures.linkedlist;
 
+import org.apache.commons.math3.ode.sampling.StepInterpolator;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LinkedListTest
 {
-  @Test void testAMethod()
+  @Test void addAString()
     {
       LinkedList sut = new LinkedList();
-      System.out.println(sut);
-      assertTrue(true);
+
+      sut.add("maddie");
+      sut.add("lincoln");
+      sut.add("truman");
+
+    assertTrue(sut.lincoln.contains("maddie"));
+    assertFalse(sut.lincoln.contains("sage"));
+    }
+
+    @Test void searchAstring() {
+    LinkedList classToSearch = new LinkedList();
+
+      java.util.LinkedList<String> test = new java.util.LinkedList<>();
+
+      test.add("truman");
+      test.add("maddie");
+      test.add("lincoln");
+
+    assertTrue(classToSearch.includes(test,"truman"));
+    assertFalse(classToSearch.includes(test, "sage"));
+
+    }
+
+    @Test void stringify(){
+      LinkedList classToSearch = new LinkedList();
+
+      java.util.LinkedList<String> test = new java.util.LinkedList<>();
+      test.add("truman");
+      test.add("lincoln");
+      test.add("eleanor");
+      test.add("maddie");
+
+      String checker = classToSearch.toStringify(test);
+
+      assertTrue(checker.contains("maddie"));
     }
 }
